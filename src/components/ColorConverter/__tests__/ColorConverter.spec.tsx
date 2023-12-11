92178a0d3746fb8d5f94bb26c708ab4371747c0d
trushchelev-ilya <freedrex@gmail.com>
2023-12-11 20:39:40 +0300

    add component tests

--- src/components/ColorConverter/__tests__/ColorConverter.spec.tsx
+++ src/components/ColorConverter/__tests__/ColorConverter.spec.tsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom';
-import {render, screen} from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
 import {ColorConverter} from '../index.tsx';
 
 describe('Color Converter', () => {
@@ -11,8 +11,53 @@ describe('Color Converter', () => {
         render(<ColorConverter />);
         expect(screen.getByLabelText('Select target color:')).toBeInTheDocument();
     });
-    it('hsl dif color block should\'t renders', () => {
+    it('hsl dif color block shouldn\'t renders', () => {
         render(<ColorConverter />);
-        expect(screen.queryByLabelText('HSL dif:')).not.toBeInTheDocument();
+        expect(screen.queryByText('HSL dif')).not.toBeInTheDocument();
+    });
+    it('hsl dif color block shouldn renders when target and base colors exist', () => {
+        render(<ColorConverter />);
+
+        const baseColorPickerInput = screen.getByLabelText('Select base color:');
+        const targetColorPickerInput = screen.getByLabelText('Select target color:');
+
+        fireEvent.input(baseColorPickerInput, {target: {value: '#333333'}});
+        expect(screen.queryByText('HSL dif')).not.toBeInTheDocument();
+        fireEvent.input(targetColorPickerInput, {target: {value: '#444444'}});
+
+        expect(screen.getByText('HSL dif')).toBeInTheDocument();
+    });
+    it('base color picker change value and bg-color', () => {
+        render(<ColorConverter />);
+
+        const colorPickerInput = screen.getByLabelText('Select base color:');
+        fireEvent.input(colorPickerInput, {target: {value: '#333333'}});
+
+        expect(colorPickerInput).toHaveValue('#333333');
+    });
+    it('base color change style', () => {
+        render(<ColorConverter />);
+
+        const colorPickerInput = screen.getByLabelText('Select base color:');
+        fireEvent.input(colorPickerInput, {target: {value: '#333333'}});
+
+        expect(colorPickerInput.closest('div')).toHaveStyle('background: #333333');
+    });
+
+    it('target color picker change value and bg-color', () => {
+        render(<ColorConverter />);
+
+        const colorPickerInput = screen.getByLabelText('Select target color:');
+        fireEvent.input(colorPickerInput, {target: {value: '#333333'}});
+
+        expect(colorPickerInput).toHaveValue('#333333');
+    });
+    it('target color change style', () => {
+        render(<ColorConverter />);
+
+        const colorPickerInput = screen.getByLabelText('Select target color:');
+        fireEvent.input(colorPickerInput, {target: {value: '#333333'}});
+
+        expect(colorPickerInput.closest('div')).toHaveStyle('background: #333333');
     });
 });
